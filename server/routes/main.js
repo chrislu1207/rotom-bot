d78036e03c9efb79e9837c28dca2fd516217bd3c
Timoteo Shockness <tshockness@atb.com>
2019-12-17 15:01:56 -0800

    I think the node in heroku cannot use async await

--- server/routes/main.js
+++ server/routes/main.js
@@ -9,18 +9,18 @@ r.get('/', (req, res, next) => {
   return res.send('WHY')
 })
 
-r.get('/test', async (req, res, next) => {
-  try {
-    const resp = await axios.post(
-      `https://hooks.slack.com/services/${config.get('slack.token')}`,
-      {
-        text: 'YO YO!!'
-      })
-    console.log(resp)
-    return res.send('okay')
-  } catch (error) {
-    return res.send('Something went really bad!')
-  }
-})
+// r.get('/test', async (req, res, next) => {
+//   try {
+//     const resp = await axios.post(
+//       `https://hooks.slack.com/services/${config.get('slack.token')}`,
+//       {
+//         text: 'YO YO!!'
+//       })
+//     console.log(resp)
+//     return res.send('okay')
+//   } catch (error) {
+//     return res.send('Something went really bad!')
+//   }
+// })
 
 module.exports = r;
