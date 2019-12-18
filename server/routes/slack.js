b2b481f1ef6f9d48f6835c408d3617a131049c74
Timoteo Shockness <tshockness@atb.com>
2019-12-18 12:38:31 -0800

    Auto Reply

--- server/routes/slack.js
+++ server/routes/slack.js
@@ -6,30 +6,18 @@ const r = express.Router()
 
 r.post('/', (req, res, next) => {
   const payload = req.body.event;
-  let text = ''
-  if(payload.user === 'URDAKB6NN'){
-    text = 'Hey handsome. Hope you doing well!! :*'
-  }
   console.log(payload)
 
-  const instance = axios.create({
-    baseURL: 'https://slack.com/api/',
-    timeout: 2000,
-    header: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Authorization': `Bearer ${process.env.BOT_TOKEN || require(config).get('bot.token')}`
-    }
-  })
-
-  instance.post('chat.postMessage', {
+  axios.post('https://slack.com/api/chat.postMessage', {
     channel: payload.channel,
-    text: text || 'asdawdsa'
-  }).then(r => {
-    console.log(r, 'Response')
+    text: 'asdawdsa'
+  }, {headers: {
+    'Content-Type': 'application/json',
+      'Authorization': `Bearer ${process.env.BOT_TOKEN || require('config').get('bot.token')}`
+  }}).then(r => {
     return res.sendStatus(200)
   }).catch(e => {
-    console.log(e, 'errrrrrorrrrr')
-    return next()
+    return next(`${e}, got an ERROR`)
   })
 })
 
