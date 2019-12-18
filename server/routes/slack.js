86b6e1a4ec811cd3793d8e77d1140cae7cb15af2
Timoteo Shockness <tshockness@atb.com>
2019-12-17 18:32:20 -0800

    Catching slack events

--- server/routes/slack.js
+++ server/routes/slack.js
@@ -3,8 +3,9 @@ const express = require('express')
 const r = express.Router()
 
 r.post('/', (req, res, next) => {
-  const challenge = req.body.challenge || 'no challenge';
-  res.send({challenge})
+  const payload = req.body.event;
+  console.log(payload)
+  res.sendStatus(200)
 })
 
 module.exports = r;
