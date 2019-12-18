9e62f72b2815f72c59a2e9f8f6b2268b5558d1fe
Timoteo Shockness <tshockness@atb.com>
2019-12-17 18:44:00 -0800

    Making bot to reply

--- server/routes/slack.js
+++ server/routes/slack.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const axios = require('axios')
 
 const r = express.Router()
 
 r.post('/', (req, res, next) => {
   const payload = req.body.event;
+  let text = ''
+  if(payload.user === 'URDAKB6NN'){
+    text = 'Hey handsome. Hope you doing well!! :*'
+  }
+
+  axios.post('https://slack.com/api/chat.postMessage', {
+    token: process.env.BOT_TOKEN || require(config).get('bot.token'),
+    channel: payload.channel,
+    text: text || 'asdawdsa'
+  })
   console.log(payload)
   res.sendStatus(200)
 })
