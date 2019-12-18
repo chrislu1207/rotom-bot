6c5be788e4948dc7e22b2a16ab5691e43d125141
Timoteo Shockness <tshockness@atb.com>
2019-12-17 16:42:44 -0800

    Middleware done

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,10 +1,6 @@
 const config = require('config')
 
 module.exports.checkToken = (req, res, next) => {
-    // Check for Sec Token on Header (Middleware) - TIM
-  //Extrat te event type
-  // Call a switch (fn) based on the event type (it will send requrest to slack)
-  // return 200
   const secret = config.get('slack.secret') || process.env.SECRET
 
   console.log(secret, "My Secret")
