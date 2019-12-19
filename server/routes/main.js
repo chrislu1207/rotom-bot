85797b38c3dd5b08c9715850093081e84e2b8458
Timoteo Shockness <tshockness@atb.com>
2019-12-18 17:39:55 -0800

    CRIIIIIIZ

--- server/routes/main.js
+++ server/routes/main.js
@@ -23,10 +23,11 @@ const axiosConstructor = (formattedMessage) => {
 
 r.post('/', checkToken, (req, res, next) => {
   const type = req.get('X-Gitlab-Event');
-  if (type === 'Merge Request' && req.body.object_attributes.work_in_progress) return res.sendStatus(200)
+  console.log(type, '- TYPE')
+  console.log(req.body, '- BODY REQ')
+  if (type === 'Merge Request Hook' && req.body.object_attributes.work_in_progress) return res.sendStatus(200)
   axios(axiosConstructor(formatMessage(req.get('X-Gitlab-Event'), req.body)))
     .then(r => {
-      console.log(r);
       return res.sendStatus(200)
     })
     .catch(e => {
